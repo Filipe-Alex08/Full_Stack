36fac4d74502c586a7027bc0d75034633a3dad6c
Filipe <Filipe.alex08@gmail.com>
2024-04-06 18:17:32 -0300

    acrescentado alguns desafios para serem feitos

--- Desafios/Python/Desafios Infinity/Desafio12.py
+++ Desafios/Python/Desafios Infinity/Desafio12.py
@@ -2,6 +2,7 @@
 Em seguida, crie uma função chamada "calcular_media" que irá receber as notas digitadas e calcular a média do aluno. 
 Também crie uma função chamada "verificar_situacao" que, com base na média calculada, irá exibir a situação do aluno: "Reprovado" se a média for menor que 7, "Aprovado" se a média for maior ou igual a 7, e "Parabéns, sua média é 10" se a média for igual a 10.
 """
+
 def calcular_media(notas):
     total_notas = sum(notas)
     quantidade_notas = len(notas)
