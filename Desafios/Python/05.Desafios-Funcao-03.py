3de17d24914999deaba6457ad48337e9b38ab1eb
Filipe <Filipe.alex08@gmail.com>
2024-04-27 15:28:33 -0300

    Refazendo a pasta

--- Desafios/Python/05.Desafios-Funcao-03.py
+++
@@ -1,5 +0,0 @@
-def numeros_pares(lista_numeros):
-    return [num for num in lista_numeros if num % 2 == 0]
-
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Números pares:", numeros_pares(numeros))
